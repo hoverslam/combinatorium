94ab8247f182aed3628ee40b4884880757aa4233
hoverslam <hoverslam@proton.me>
2024-03-23 22:35:04 +0100

    Rename folder

---
+++ combinatorium/agents/__init__.py
@@ -0,0 +1,11 @@
+from combinatorium.agents.human import HumanAgent
+from combinatorium.agents.simple import MinimaxAgent, AlphaBetaAgent, NegamaxAgent
+from combinatorium.agents.random import RandomAgent
+
+__all__ = [
+    "RandomAgent",
+    "MinimaxAgent",
+    "AlphaBetaAgent",
+    "NegamaxAgent",
+    "HumanAgent",
+]
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from combinatorium import ConnectFour, TicTacToe
-from combinatorium.agent import RandomAgent, MinimaxAgent, AlphaBetaAgent
+from combinatorium.agents import RandomAgent, MinimaxAgent, AlphaBetaAgent
 
-p1 = RandomAgent()
+p1 = AlphaBetaAgent(5)
 p2 = RandomAgent()
 g = ConnectFour(p1, p2)
 g.reset()
