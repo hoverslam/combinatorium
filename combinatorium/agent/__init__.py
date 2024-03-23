9cde932d668ec9aa23b2c385b5fdb5060fe5a694
hoverslam <hoverslam@proton.me>
2024-03-23 17:31:12 +0100

    Add negamax agent

--- combinatorium/agent/__init__.py
+++ combinatorium/agent/__init__.py
@@ -1,10 +1,11 @@
 from combinatorium.agent.human import HumanAgent
-from combinatorium.agent.simple import MinimaxAgent, AlphaBetaAgent
+from combinatorium.agent.simple import MinimaxAgent, AlphaBetaAgent, NegamaxAgent
 from combinatorium.agent.random import RandomAgent
 
 __all__ = [
     "RandomAgent",
     "MinimaxAgent",
     "AlphaBetaAgent",
+    "NegamaxAgent",
     "HumanAgent",
 ]
